73c181520d213f93307ad9079076836b7b8a1732
JorgitoR <jorgitouribe133@gmail.com>
2020-11-11 21:34:35 -0500

    Dm Direct Message

--- Dm/admin.py
+++ Dm/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Canal, CanalUsuario, CanalMensaje
+
+class CanalMensajeInline(admin.TabularInline):
+	model = CanalMensaje
+	extra = 1
+
+class CanalUsuarioInline(admin.TabularInline):
+	model = CanalUsuario
+	extra = 1
+
+
+class CanalAdmin(admin.ModelAdmin):
+	inlines = [CanalMensajeInline, CanalUsuarioInline]
+
+	class Meta:
+		model = Canal
+
+admin.site.register(Canal, CanalAdmin)
+admin.site.register(CanalUsuario)
+admin.site.register(CanalMensaje)
